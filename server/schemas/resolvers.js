d3c714fd5af543abd1471fa11acf590c940aa828
Egg <adrianjimenez1950@gmail.com>
2022-12-05 04:54:12 -0500

    Added checkout functionality

--- server/schemas/resolvers.js
+++ server/schemas/resolvers.js
@@ -1,6 +1,7 @@
 const { AuthenticationError } = require('apollo-server-express');
 const { User, Product, Category, Order } = require('../models');
 const { signToken } = require('../utils/auth');
+const stripe = require('stripe')('sk_test_4eC39HqLyjWDarjtT1zdp7dc');
 
 const resolvers = {
   Query: {
@@ -50,6 +51,51 @@ const resolvers = {
       }
 
       throw new AuthenticationError('Not logged in');
+    },
+    checkout: async (parent, args, context) => {
+      // url doesn't work with apollo studio ! 
+      const url = new URL(context.headers.referer).origin;
+      const productArr = [];
+
+      for(i = 0; i < args.products.length; i++) {
+        console.log(args.products[i]);
+        const product = await Product.findById(args.products[i]).populate();
+        productArr.push(product);
+      }
+      
+      const { products } = new Order({ products: productArr });
+      // const { products } = await order.populate('products');
+      const line_items = [];
+
+      for(let i=0; i < products.length; i++) {
+        const product = await stripe.products.create({
+          name: products[i].name,
+          description: products[i].description,
+          images: [`${url}/images/${products[i].image}`]
+        });
+
+        const price = await stripe.prices.create({
+          product: product.id,
+          unit_amount: products[i].price * 100,
+          currency: 'usd',
+        });
+
+        line_items.push({
+          price: price.id,
+          quantity: 1
+        })
+      }
+
+      console.log(line_items);
+      const session = await stripe.checkout.sessions.create({
+        payment_method_types: ['card'],
+        line_items,
+        mode: 'payment',
+        success_url: `${url}/success?session_id={CHECKOUT_SESSION_ID}`,
+        cancel_url: `${url}/`
+      });
+
+      return { session: session.id };
     }
   },
   Mutation: {
